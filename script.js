11dbb9dd487db70f4418f49fe65e58f0ce8a498c
Paul Alexandru <52256085+paul7599@users.noreply.github.com>
2020-09-25 15:12:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+
+pig-game-dice
+
+Show
+script.js
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+var lastDice;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        // 1. Random number
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+
+        //2. Display the result
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'https://cdn.glitch.com/c818dfc4-92eb-4120-90d9-d82eeb706cda%2Fdice-' + dice1 + '.png?v=1564677406506';
+        document.getElementById('dice-2').src = 'https://cdn.glitch.com/c818dfc4-92eb-4120-90d9-d82eeb706cda%2Fdice-' + dice2 + '.png?v=1564677406506';
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        
+        if (dice1 === 6 && dice2 === 6) {
+            //Player looses score
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
+        } else if (dice1 !== 1 && dice2 !== 1) {
+            //Add score
+            roundScore += dice1 + dice2;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+        
+    }    
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        
+        // Undefined, 0, null or "" are COERCED to false
+        // Anything else is COERCED to true
